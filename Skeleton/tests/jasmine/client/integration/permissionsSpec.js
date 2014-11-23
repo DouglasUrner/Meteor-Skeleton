c9f8b666096af59b1a4ab85024661f49d875841f
Douglas Urner <dlu@canishe.com>
2014-11-23 02:10:09 -0800

    Create first tests, but disabled testing - currently tests cause app to crash.

---
+++ Skeleton/tests/jasmine/client/integration/permissionsSpec.js
@@ -0,0 +1,36 @@
+File: tests/jasmine/client/integration/premissionsSpec.js
+
+"use strict";
+
+describe('Permision Checks:', function () {
+
+  it('anonymous users can see public pages.', function () {
+
+  });
+
+  it('logged in users can see public pages.', function () {
+
+  });
+
+
+  it('anonymous users can not see private pages.', function () {
+
+  });
+
+  it('logged in users can see private pages.', function () {
+
+  });
+
+  it('annoymous users can not see administrative pages.', function () {
+
+  });
+
+  it('logged in users without the "admin" role can not see adminstrative pages', function () {
+
+  });
+
+  it('logged in users with the "admin" role can see administrative pages.',
+    function () {
+
+  });
+});
--- meteor-skeleton.sh
+++ meteor-skeleton.sh
@@ -1,6 +1,8 @@
-#meteor create $1
-#cd $1
-#rm $1.{css,html,js}
-#mkdir client lib server tests
+if [ ! -d $1 ] ; then
+	meteor create $1
+	cd $1
+	rm $1.{css,html,js}
+	mkdir client lib server tests
+fi
 
-meteor add sanjo:jasmine velocity:html-reporter
+#meteor add sanjo:jasmine velocity:html-reporter
